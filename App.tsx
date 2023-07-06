bf3672e8c250211c2a36d123758da4670ddc1591
shravankumarkolanupaka <shravankumar.lgit@gmail.com>
2023-07-06 21:29:50 +0530

    Added customslidercomp

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import CustomSliderComp from './Components/CustomSliderComp'
+
+
+const App = () => {
+  return (
+    <View>
+        <CustomSliderComp/>
+    </View>
+  )
+}
+
+export default App
